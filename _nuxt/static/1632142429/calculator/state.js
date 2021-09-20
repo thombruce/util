64f34076bedbfae96f5eb2a83f4f96dfb133008e
thombruce <thombruce@users.noreply.github.com>
2021-09-20 12:58:28 +0000

    deploy: 4191dcc39fa285331131be8a1cc7e6c1ad451cac

--- _nuxt/static/1632142429/calculator/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632142429",layout:"docs",error:a,serverRendered:true,routePath:"\u002Fcalculator",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"aa7ec00f"}}}}(null));
